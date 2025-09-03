d2df7b31fec95386767b2a7a825bbb0eb76805d5
wifo08001 <wifo08001@E-PW04SM1S>
2025-09-03 13:14:07 +0200

    Add project files.

---
+++ Inlämning_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Inlämning_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("Du har virus lol");
+            Console.ReadKey();
+        }
+
+
+    }
+}
